db06edea0a20736d01d8019c00e0eba10d1e9500
Jules <julia.poo.poo.poo@gmail.com>
2024-06-27 18:23:25 +0800

    fix jax top_k

--- array_api_compat/jax/__init__.py
+++ array_api_compat/jax/__init__.py
@@ -10,15 +10,6 @@ def top_k(
     *,
     largest=True,
 ):
-
-    # `swapaxes` is used to implement
-    # the `axis` kwarg
-    # x = numpy.swapaxes(x, axis, -1)
-    # vals, args = lax.top_k(x, k)
-    # vals = numpy.swapaxes(vals, axis, -1)
-    # args = numpy.swapaxes(args, axis, -1)
-    # return vals, args
-
     # The largest keyword can't be implemented with `jax.lax.top_k`
     # efficiently so am using `jax.numpy` for now
     if k <= 0:
